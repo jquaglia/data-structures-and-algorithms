e165b50d8807c092ea89c997ff59feea052a9a58
jquaglia <jqugali@gmail.com>
2021-01-18 14:29:34 -0800

    completed problem 2

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -96,7 +96,8 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 
 const getCourseKeys = (obj) => {
   // Solution code here...
-  
+  const keyArr = Object.keys(obj);
+  return keyArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
