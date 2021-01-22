e9ae606b02cb5021bb699ea15540b489cf4336a5
jquaglia <jqugali@gmail.com>
2021-01-22 15:06:11 -0800

    completed problem 3

--- code-challenges/challenges-10.test.js
+++ code-challenges/challenges-10.test.js
@@ -61,6 +61,10 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 
 const totalSum = (input) => {
   // Solution code here...
+  const reduce = input.reduce((acc, value) => acc.concat(value), []);
+  console.log(reduce);
+  const sum = reduce.reduce((acc, value) => (acc + value), 0);
+  return sum;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -217,7 +221,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add all the numbers in the arrays', () => {
     const nums = [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]];
 
